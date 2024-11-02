90e843336a0da23f8950b70b12f88e1cc26fa84b
thoi2 <kinggun1505@naver.com>
2024-11-03 00:12:54 +0900

    Add README 1. 1 ~ 45 까지 중복되지 않는 정수 6개 반환 기능 Add ReadMe Lotto 클래스 getter, toString 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(new Lotto(lottoNumberGenerator()));
+
+    }
+
+    public static List<Integer> lottoNumberGenerator(){
+        return Randoms.pickUniqueNumbersInRange(1,45,6);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,5 @@
 package lotto;
-
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +12,21 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
+    @Override
+    public String toString(){
+        return numbers.toString();
+    }
+
+
+
+
 
     // TODO: 추가 기능 구현
+
 }
