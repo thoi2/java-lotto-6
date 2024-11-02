e09d807df77f049a4242ef2e7cd4880f5e2f6566
thoi2 <kinggun1505@naver.com>
2024-11-03 01:06:13 +0900

    Add README 6. 로또 구입 금액 입력 기능 Add README 7. 총 구매 로또 개수 계산 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,31 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println(new Lotto(lottoNumberGenerator()));
+        System.out.println("구입금액을 입력해 주세요.");
+        String unCheckedPayMoney = Console.readLine();
+        int payMoney = checkPayMoney(unCheckedPayMoney);
+        int lottoPurchasesNum = calculateLottoPurchasesNum(payMoney);
+        System.out.println(lottoPurchasesNum+"개를 구매했습니다.");
+
 
     }
 
     public static List<Integer> lottoNumberGenerator(){
-        return Randoms.pickUniqueNumbersInRange(1,45,6);
+        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1,45,6);
+        return lottoNumber;
     }
+    public static int checkPayMoney(String unCheckedPayMoney){
+        int checkPayMoney = Integer.parseInt(unCheckedPayMoney);
+        return checkPayMoney;
+    }
+    public static int calculateLottoPurchasesNum (int payMoney){
+        int lottoPurchasesNum = payMoney/1000;
+        return lottoPurchasesNum;
+    }
+
 }
