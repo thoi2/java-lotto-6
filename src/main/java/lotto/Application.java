aa399bae399604f0e83922d8611223813d87f308
thoi2 <kinggun1505@naver.com>
2024-11-04 14:44:06 +0900

    Add README 12 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,9 +7,9 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Application {
+    private static final int PRICEPERLOTTO = 1000;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         int payMoney = getPayMoney();
         int lottoPurchasesNum = calculateLottoPurchasesNum(payMoney);
         List<Lotto> myLottoNum = myLottoNum(lottoPurchasesNum);
@@ -23,22 +23,33 @@ public class Application {
         return lottoNumber;
     }
     public static int getPayMoney(){
-        System.out.println("구입금액을 입력해 주세요.");
-        String unCheckedPayMoney = Console.readLine();
-        int payMoney = checkPayMoney(unCheckedPayMoney);
-        return payMoney;
-
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String unCheckedPayMoney = Console.readLine();
+                int payMoney = checkPayMoney(unCheckedPayMoney);
+                return payMoney;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 구입 금액은 정수만 입력가능합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
+
     public static int checkPayMoney(String unCheckedPayMoney){
         int checkPayMoney = Integer.parseInt(unCheckedPayMoney);
+        if (checkPayMoney < PRICEPERLOTTO || checkPayMoney % PRICEPERLOTTO > 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은 1000으로 나누어 떨어져야합니다.");
+        }
         return checkPayMoney;
     }
-    public static int calculateLottoPurchasesNum (int payMoney){
-        final int pricePerLotto=1000;
-        int lottoPurchasesNum = payMoney/pricePerLotto;
-        System.out.println(lottoPurchasesNum+"개를 구매했습니다.");
+    public static int calculateLottoPurchasesNum (int payMoney) {
+        int lottoPurchasesNum = payMoney / PRICEPERLOTTO;
+        System.out.println(lottoPurchasesNum + "개를 구매했습니다.");
         return lottoPurchasesNum;
     }
+
     public static List<Lotto> myLottoNum(int lottoPurchasesNum){
         List<Lotto> myLottoNum = new ArrayList<>();
         for(int i=1;i<lottoPurchasesNum+1;i++ ){
@@ -49,12 +60,19 @@ public class Application {
         return myLottoNum;
     }
     public static List<Integer> getLuckyNum(){
-        System.out.println("\n당첨 번호를 입력해 주세요.");
-        String unCheckedLuckyNumber = Console.readLine();
-        List<Integer> luckyNum = checkLuckyNum(unCheckedLuckyNumber);
-        new Lotto(luckyNum);
-        return luckyNum;
-
+        while (true) {
+            try {
+                System.out.println("\n당첨 번호를 입력해 주세요.");
+                String unCheckedLuckyNumber = Console.readLine();
+                List<Integer> luckyNum = checkLuckyNum(unCheckedLuckyNumber);
+                new Lotto(luckyNum);
+                return luckyNum;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 로또 번호는 정수만 입력가능합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
     public static List<Integer> checkLuckyNum(String unCheckedLuckyNumber){
         StringTokenizer tokenizer = new StringTokenizer(unCheckedLuckyNumber,",");
@@ -66,13 +84,24 @@ public class Application {
         return checkLuckyNum;
     }
     public  static int getBonusNumber(){
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        String unCheckedBonusNumber = Console.readLine();
-        int bonusNumber=checkBonusNumber(unCheckedBonusNumber);
-        return bonusNumber;
+        while (true) {
+            try {
+                System.out.println("\n보너스 번호를 입력해 주세요.");
+                String unCheckedBonusNumber = Console.readLine();
+                int bonusNumber = checkBonusNumber(unCheckedBonusNumber);
+                return bonusNumber;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 정수만 입력가능합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
     public static int checkBonusNumber(String unCheckedBonusNumber) {
         int checkBonusNumber = Integer.parseInt(unCheckedBonusNumber);
+        if (checkBonusNumber < 0 || checkBonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
         return checkBonusNumber;
     }
 }
